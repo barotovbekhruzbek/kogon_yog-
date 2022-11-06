d727a277777897a666f2b88819aaf041b69ce951
barotovbekhruzbek <barotovbekhruzbek@gmail.com>
2022-11-06 17:03:14 +0500

    changed all file

--- header.php
+++ header.php
@@ -45,13 +45,13 @@
                                     <ul class="navigation clearfix">
                                         <li class="dropdown"><a href="#">Jamiyat xaqida</a>
                                             <ul>
-                                                <li><a href="document/Жамият%20хакида.pdf">Jamiyat xaqida++</a></li>
+                                                <li><a href="document/Жамиятхакида.pdf">Jamiyat xaqida++</a></li>
                                                 <li><a href="document/maqsad.pdf">Jamiyatning predmeti va maqsadi++</a></li>
                                                 <li><a href="document/raxbariyat.pdf">Raxmariyat++</a></li>
                                                 <li><a href="product.php">Ishlab chiqarilayotgan maxsulotlar xaqida++</a></li>
                                                 <li><a href="doc1.php">Bo`sh ish urinlari</a></li>
                                                 <li><a href="document/qabul.pdf">Qabul kunlari++</a></li>
-                                                <li><a href="doc1.php">Ommaviy tadbirlar</a></li>
+                                                <li><a href="ommaviy_tadbirlar.php">Ommaviy tadbirlar ++</a></li>
                                                 <li><a href="document/Ривожланиш%20стратегияси.pdf">Rivojlanish strategiyasi++</a></li>
                                                 <li><a href="#">Tarkibiy bo`linma</a></li>
 
@@ -62,12 +62,12 @@
                                         <li class="dropdown"><a href="#">Korparativ boshqaruv</a>
                                             <ul>
                                             <li><a href="doc1.php">Boshqaruv organlari</a></li>
-                                                    <li><a href="#">--Kuzatuv Kengashi </a></li>
-                                                    <li><a href="#">--Ijro Organi </a></li>
-                                                    <li><a href="#">--Ichki audit xizmati </a></li>
+                                                    <li><a href="document/Кузатув%20кенгаши.pdf">--Kuzatuv Kengashi ++</a></li>
+                                                    <li><a href="document/Ижроия%20органи.pdf">--Ijro Organi++ </a></li>
+                                                    <li><a href=""document/AUDIT.pdf>--Ichki audit xizmati ++</a></li>
                                                     <li><a href="document/Бошқарув_органлар_томонидан_тузилган_комиссиялар,_кенгашлар,_қўмиталар.pdf">--Boshqaruv organlar tomonidan tuzilgan (komissiyalar, kengashlar, qo`mitalar)++</a></li>
                                                 <li><a href="doc1.php">ichki xujjatlar</a></li>
-                                                <li><a href="#">--Ustav </a></li>
+                                                <li><a href="document/УСТАВ%202018.pdf">--Ustav++ </a></li>
                                                 <li><a href="#">--Nizomlar </a></li>
                                                 <li><a href="document/Корпоратив%20Бошкарув%20Кодекси.pdf">--Korporativ boshqaruv kodeksi++ </a></li>
                                                 <li><a href="document/aksiyador.pdf">Aksiyadorlar++</a></li>
@@ -82,24 +82,24 @@
                                             <ul>
                                                 <li><a href="doc1.php">Oshkora malumotlar</a></li>
                                                 <li><a href="document/Мухим_факт_№8_Ижроия_органи_таркибидаги_узгаришлар.pdf">--Muxim faktlar++</a></li>
-                                                <li><a href="doc1.php">--Yillik xisobotlar</a></li>
-                                                <li><a href="choraklik_xisobot.php">--Choraklik xisobotlar</a></li>
+                                                <li><a href="yillik_xisobot.php">--Yillik xisobotlar++</a></li>
+                                                <li><a href="choraklik_xisobot.php">--Choraklik xisobotlar++</a></li>
                                                 <li><a href="document/Асосий%20курсаткичлар%20-%20копия.pdf">--Asosiy ko`rsatgichlar to`g`risida++</a></li>
-                                                <li><a href="doc1.php">--Audit xulosalari</a></li>
+                                                <li><a href="audit_xulosasi.php">--Audit xulosalari++</a></li>
 
-                                                <li><a href="doc1.php">umumiy yig`ilish qarorlari</a></li>
+                                                <li><a href="document/2022_йил_18_август_Жамият_акциядорларининг_навбатдаги_умумий_йиғилиши.pdf">umumiy yig`ilish qarorlari++</a></li>
                                                 <li><a href="doc/b_plan_2021.pdf">Biznes reja 2021 +++</a></li>
                                                 <li><a href="doc/b_plan_2022.pdf">Biznes reja 2022 +++</a></li>
                                                 <li><a href="doc/b_plan_all.pdf">Biznes reja 21-22-2023 +++</a></li>
                                                 <li><a href="doc/dvident_uz.docx">Dvidentlar+++</a></li>
                                                 <li><a href="emissiyalar.php">Emissiya risolasi +++</a></li>
 
-                                                <li><a href="doc1.php">Litsenziya va sertiifikatlar</a></li>
+                                                <li><a href="litsenziya.php">Litsenziya va sertiifikatlar++</a></li>
                                             </ul>
                                         </li>                                        
                                         <li class="dropdown"><a href="">Yangiliklar</a>
                                             <ul>
-                                                <li><a href="aboute.php">AUY o`tqazilishi xaqida</a></li>
+                                                <li><a href="document/Навбатдан_ташқари_йигилиш_ўтказилиши_тўғрисида.pdf">AUY o`tqazilishi xaqida++</a></li>
                                                 <li><a href="doc/konkurs.pdf" target="_blink">Tandovlar va tendirlar+++</a></li>
                                             </ul>
                                         </li>
@@ -183,7 +183,7 @@
                                                 <li><a href="doc1.php">--Asosiy ko`rsatgichlar to`g`risida</a></li>
                                                 <li><a href="doc1.php">--Audit xulosalari</a></li>
 
-                                                <li><a href="doc1.php">umumiy yig`ilish qarorlari</a></li>
+                                                <li><a href="document/2022_йил_18_август_Жамият_акциядорларининг_навбатдаги_умумий_йиғилиши.doc">umumiy yig`ilish qarorlari++</a></li>
                                                 <li><a href="doc/b_plan_2021.pdf">Biznes reja 2021 +++</a></li>
                                                 <li><a href="doc/b_plan_2022.pdf">Biznes reja 2022 +++</a></li>
                                                 <li><a href="doc/b_plan_all.pdf">Biznes reja 21-22-2023 +++</a></li>
--- litsenziya.php
+++ litsenziya.php
@@ -1,1 +1,217 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <?php
+    include "head.php"
+    ?>
+</head>
+
+<body class="page-wrapper">
+
+
+
+<header class="main-header">
+
+    <?php
+    include "header.php"
+    ?>
+
+
+</header>
+
+
+<main>
+    <div class="container">
+        <div class="part-4">
+            <div class="main-text">
+                <h2> Litsenziya va Sertifikatlar </h2>
+            </div>
+            <div class="wrapper">
+                <div class="card">
+                    <div class="icon-file">
+                        <a href="document/sertifikat/УЗБЕКИСТОН%20РЕСПУБЛИКАСИ.pdf">
+                            <i class="fa fa-file" aria-hidden="true"></i>
+                            <p>2021 -yil O`zbekiston Respublikasi Sog`liqni saqlash vazirligi </p>
+                            <button class="btn-danger"> ko`rish </button>
+                        </a>
+                    </div>
+                </div>
+                <!--         2              -->
+                <div class="card">
+                    <div class="icon-file">
+                        <a href="document/sertifikat/НАЦИОНАЛЬНАЯ_СИСТЕМА_СЕРТИФИКАЦИИ.pdf">
+                            <i class="fa fa-file" aria-hidden="true"></i>
+                            <p>2022- yil O`zbekiston Respublikasi Milliy Sertifikati</p>
+                            <button class="btn-danger"> ko`rish </button>
+                        </a>
+                    </div>
+                </div>
+
+                <!--        3            -->
+                <div class="card">
+                    <div class="icon-file">
+                        <a href="document/sertifikat/НАЦИОНАЛЬНАЯ_СИСТЕМА_СЕРТИФИКАЦИИ_РЕСПУБЛИКИ_УЗБЕКИСТАН.pdf">
+                            <i class="fa fa-file" aria-hidden="true"></i>
+                            <p>2019 - yil O`zbekiston Respublikasi Milliy Sertifikati</p>
+                            <button class="btn-danger"> ko`rish </button>
+                        </a>
+                    </div>
+                </div>
+
+
+
+
+                <div class="card">
+                    <div class="icon-file">
+                        <a href="document/sertifikat/НАЦИОНАЛЬНАЯ_СИСТЕМА_СЕРТИФИКАЦИИ_РЕСПУБЛИКИ_УЗБЕКИСТАН.pdf">
+                            <i class="fa fa-file" aria-hidden="true"></i>
+                            <p>2019 - yil O`zbekiston Respublikasi Milliy Sertifikati</p>
+                            <button class="btn-danger"> ko`rish </button>
+                        </a>
+                    </div>
+                </div>
+
+
+
+
+            </div>
+
+
+            <div class="wrapper">
+                <div class="card">
+                    <div class="icon-file">
+                        <a href="document/sertifikat/УЗБЕКИСТОН%20РЕСПУБЛИКАСИ.pdf">
+                            <i class="fa fa-file" aria-hidden="true"></i>
+                            <p>2021 -yil O`zbekiston Respublikasi Sog`liqni saqlash vazirligi </p>
+                            <button class="btn-danger"> ko`rish </button>
+                        </a>
+                    </div>
+                </div>
+                <!--         2              -->
+                <div class="card">
+                    <div class="icon-file">
+                        <a href="document/sertifikat/НАЦИОНАЛЬНАЯ_СИСТЕМА_СЕРТИФИКАЦИИ.pdf">
+                            <i class="fa fa-file" aria-hidden="true"></i>
+                            <p>2022- yil O`zbekiston Respublikasi Milliy Sertifikati</p>
+                            <button class="btn-danger"> ko`rish </button>
+                        </a>
+                    </div>
+                </div>
+
+                <!--        3            -->
+                <div class="card">
+                    <div class="icon-file">
+                        <a href="document/sertifikat/НАЦИОНАЛЬНАЯ_СИСТЕМА_СЕРТИФИКАЦИИ_РЕСПУБЛИКИ_УЗБЕКИСТАН.pdf">
+                            <i class="fa fa-file" aria-hidden="true"></i>
+                            <p>2019 - yil O`zbekiston Respublikasi Milliy Sertifikati</p>
+                            <button class="btn-danger"> ko`rish </button>
+                        </a>
+                    </div>
+                </div>
+
+
+
+
+                <div class="card">
+                    <div class="icon-file">
+                        <a href="document/sertifikat/НАЦИОНАЛЬНАЯ_СИСТЕМА_СЕРТИФИКАЦИИ_РЕСПУБЛИКИ_УЗБЕКИСТАН.pdf">
+                            <i class="fa fa-file" aria-hidden="true"></i>
+                            <p>2019 - yil O`zbekiston Respublikasi Milliy Sertifikati</p>
+                            <button class="btn-danger"> ko`rish </button>
+                        </a>
+                    </div>
+                </div>
+
+
+
+
+            </div>
+
+        </div>
+    </div>
+</main>
+
+
+<footer class="main-footer sec-pad">
+    <?php
+    include "footer.php"
+    ?>
+</footer>
+
+
+
+<style >
+    main .part-4 h2 {
+        color: #000000;
+        text-align: center;
+    }
+    .wrapper {
+        display: flex;
+        padding-top: 80px;
+        gap: 30px;
+
+    }
+    .card {
+        width: 350px;
+        height: 350px;
+        background: #f5f2f2;
+        margin-bottom: 35px;
+        border-radius: 8px;
+
+
+    }
+    .card:hover {
+        /*box-shadow: 10px 10px 5px lightblue;*/
+        -webkit-box-shadow: 17px 14px 14px -12px rgba(34, 60, 80, 0.6);
+        -moz-box-shadow: 17px 14px 14px -12px rgba(34, 60, 80, 0.6);
+        box-shadow: 17px 14px 14px -12px rgba(34, 60, 80, 0.6);
+        transition: 0.5s linear;
+
+
+    }
+    .card .icon-file a {
+        padding-top: 40px;
+        display: flex;
+        flex-direction: column;
+        text-decoration: none;
+        align-items: center;
+    }
+    .card .icon-file  i{
+        margin-top:40px ;
+        font-size: 125px;
+        color: #ac2626;
+
+    }
+    .card .icon-file p {
+        padding-top: 80px;
+        font-size: 18px;
+        color: #000000;
+        text-align: center;
+    }
+    .card .icon-file button {
+        margin-top: 20px;
+        padding: 10px 60px;
+        border-radius: 8px;
+    }
+    @media (max-width: 800px) {
+        .wrapper {
+            flex-direction: column;
+            align-items: center;
+        }
+    }
+</style>
+
+
+
+<script type="text/javascript" src="js/jquery-2.1.4.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/wow.js"></script>
+<script src="js/validation.js"></script>
+<script src="js/jquery-ui.js"></script>
+<script src="js/bxslider.js"></script>
+<script type="text/javascript" src="js/SmoothScroll.js"></script>
+<script type="text/javascript" src="js/jQuery.style.switcher.min.js"></script>
+<script src="js/script.js"></script>
+
+</body>
